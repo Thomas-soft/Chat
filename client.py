f6eabe835fe0a49b9f6c6aeb787fdc3f363bd329
unknown <dev.thomas.soft+git@gmail.com>
2022-10-21 13:53:45 +0200

    Fonctionnel

---
+++ client.py
@@ -0,0 +1,66 @@
+import socket
+import threading
+import signal
+
+signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+HOST, PORT = "localhost", 8989
+FORMAT = "UTF-8"
+
+user_name = input("> Your name: ")
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST, PORT))
+
+try:
+    socket.send(user_name.encode(FORMAT))
+except ConnectionRefusedError:
+    print("Error: Server OFF")
+
+def sending():
+    running = True
+    while running:
+
+        try:
+            message_input = input("").encode(FORMAT)
+        except EOFError:
+            print("Error: Leaving")
+            running = False
+            break
+
+        try:
+            socket.send(message_input)
+        except OSError:
+            print("Error: SERVER OFF, message not sent")
+            running = False
+            break
+
+    socket.close()
+
+
+def recving():
+    running = True
+    s_data = ""
+    while running:
+
+        try:
+            s_data = socket.recv(500).decode(FORMAT)
+        except ConnectionAbortedError:
+            running = False
+            print("Error: Leaving")
+            break
+        except ConnectionResetError:
+            print("Error: SERVER OFF, battery is low")
+            break
+
+        if s_data == "quit":
+            running = False
+            print("Error: SERVER OFF, is shutdown")
+            break
+
+        print(s_data)
+
+    socket.close()
+
+threading.Thread(target=sending).start()
+threading.Thread(target=recving).start()
---
+++ server.py
@@ -0,0 +1,129 @@
+import socket
+import threading
+import os
+
+HOST, PORT = "localhost", 8989
+FORMAT = "UTF-8"
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind((HOST, PORT))
+socket.listen()
+os.system("cls")
+print("> Server ON <")
+
+global clients
+server_on = True
+stop_thread = False
+clients = []
+instructions = """
+0: Shutdown server
+1: Number of clients connected
+2 <username>: Remove a client
+3: List of connected clients
+"""
+
+def handling(user_name, s_client, h_client):
+
+    for i in clients:
+        if i[1] != s_client:
+            i[1].send(f"{user_name} has joined!".encode(FORMAT))
+
+    running = True
+    while running:
+
+        try:
+            c_data = s_client.recv(500).decode(FORMAT)
+        except ConnectionResetError:
+            running = False
+            break
+        except ConnectionAbortedError:
+            running = False
+            break
+
+        print(f"{user_name}: {c_data}")
+
+        for i in clients:
+            if i[1] == s_client:
+                if i[-1] == True:
+                    running = False
+            else:
+                i[1].send(f"{user_name}: {c_data}".encode(FORMAT))
+
+    for i in clients:
+        if i[1] != s_client:
+            try:
+                i[1].send(f"Server: {user_name} has left...".encode(FORMAT))
+            except OSError:
+                pass
+
+    s_client.close()
+    for i in clients:
+        if i[1] == s_client:
+            i[1].close()
+            clients.remove(i)
+
+
+def admin():
+    global clients
+    running = True
+    while running:
+        try:
+            user_input = input("")
+        except EOFError:
+            running = False
+            break
+
+        os.system("cls")
+        print(instructions)
+
+        if user_input == '0':
+            user_input = input("> Are you sure? (Y/n): ").lower()
+            if user_input == '' or user_input == 'y' or user_input == 'yes': 
+                for i in clients:
+                    i[-1] = True
+                running = False
+            else:
+                print("> Shutdown canceled. <")
+
+        elif user_input == '1':
+            print(f"> {len(clients)} of clients in server <")
+        
+        elif user_input == f"2 {user_input[2:]}":
+            for i in clients:
+                if i[0] == user_input[2:]:
+                    i[1].send("Server: Disconnected by admin!".encode(FORMAT))
+                    i[1].close()
+                    print(f"> {i[0]} is banned! <")
+
+        elif user_input == '3':
+            for i in clients:
+                print(i[0])
+
+    socket.close()
+
+
+threading.Thread(target=admin).start()
+
+print(instructions)
+
+while server_on:
+
+    try:
+        s_client, h_client = socket.accept()
+    except OSError:
+        for i in clients:
+            i[1].close()
+        print("> Server shutdown... <")
+        server_on = False
+        break
+    except KeyboardInterrupt:
+        for i in clients:
+            i[1].close()
+        print("> Server shutdown... <")
+        server_on = False
+        break
+    user_name = s_client.recv(24).decode(FORMAT)
+    clients.append([user_name, s_client, h_client, stop_thread])
+    threading.Thread(target=handling, args=(user_name, s_client, h_client)).start()
+
+print("> Server is OFF <")
